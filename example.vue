ac6d33241c10bc91d11a906273dd9a2fda0b3053
Matthias Sommer <sommer.external@infineon.com>
2022-05-04 08:06:13 +0200

    add Vue example

---
+++ example.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <Button @click="checkBrowser"> Test </Button>
+    </div>
+</template>
+
+<script lang="ts">
+import checkAndOpenUri from './protocolcheck';
+
+export default {
+    name: 'detection-example',
+
+    methods: {
+        checkBrowser(): void {
+            checkAndOpenUri(
+                "mailto:",
+                this.failCallback,
+                this.sucessCallback,
+                this.unsupportedBrowser,
+            );
+        },
+        sucessCallback(): void {
+            console.warn("failed");
+        },
+        failCallback(): void {
+            console.warn("failed");
+        },
+        unsupportedBrowser(): void {
+            console.warn("unsupported");
+        },
+    }
+};
+</script>
